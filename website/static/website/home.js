c7afbdcb2ca94645f8be2aa236343c0c5e06123c
David Voong <voong.david@gmail.com>
2018-03-22 12:17:41 -0600

    style tooltip for balance chart

--- website/static/website/home.js
+++ website/static/website/home.js
@@ -109,15 +109,19 @@ function ToolTip(){
             .duration(200)
             .style("opacity", .9);
 	that.tooltip.style("opacity", 1);
-	that.tooltip.html(d.date + ": £" + d.balance)
+	that.tooltip.html("<b>" + d.date + "</b>" + "<br>£" + d.balance.toFixed(2))
             .style("left", (d3.event.pageX) + "px")
             .style("top", (d3.event.pageY - 28) + "px");
+	d3.select(this)
+	    .style('opacity', 0.6)
     };
 
     this.mouseout_callback = function(d){
 	that.tooltip.transition()
             .duration(500)
             .style("opacity", 0);
+	d3.select(this)
+	    .style('opacity', 1)
     };
     
 }
